12f78d056a0f65451ddfa533a5049c3a9583019c
ktownag <42381296+ktownag@users.noreply.github.com>
2020-10-11 23:30:29 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 rm -rf elecV2P
 git clone https://github.com/elecV2/elecV2P.git
 cd elecV2P
-apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-curl -sL https://deb.nodesource.com/setup_12.x | -E bash -
-apt-get install -y nodejs
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+sudo curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs yarn
 yarn
 yarn start
